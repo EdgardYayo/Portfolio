c5380871236f079960cdc6a0305a0dcf8c147eb6
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-02-17 21:52:41 -0800

    adding new certificade

--- pages/certificates/index.js
+++ pages/certificates/index.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import s from "../../styles/Certificates.module.css";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import cert1 from '../../public/imagenes/cert1.jpeg';
 import cert2 from '../../public/imagenes/cert2.png';
 import cert3 from '../../public/imagenes/cert3.jpeg';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import cert4 from '../../public/imagenes/1f89271d13da0d9158361fa5a5cbf1cf6d56a1beb14ac86492b142332e96efc0_page-0001.jpg';
 
 export default function Certificates(){
 
@@ -16,6 +15,7 @@ export default function Certificates(){
                 <h1 className={s.title}>Certificates</h1>
             </div>
             <section className={s.contImgs}>
+                    <Image src={cert4} alt='Henry Certificade' width={400} height={500}/>
                     <Image src={cert1} alt='HTML and CSS' width={400} height={500}/>
                     <Image src={cert2} alt='Teaching Assistant' width={400} height={500}/>
                     <Image src={cert3} alt='English certificate' width={400} height={500}/>
